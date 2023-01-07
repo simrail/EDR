e66d0068389d8617f308adeee3b4b2129d58516c
Peter Mohacsi <mpeter007@gmail.com>
2023-01-07 17:39:55 +0100

    Converted backend to Typescript

---
+++ backend/src/serverController.ts
@@ -0,0 +1,28 @@
+import { simrailClient } from "./simrailClient";
+import { BASE_SIMRAIL_API } from "./config";
+import express from "express";
+import { ApiResponse, Server, Station, Train } from "@simrail/types";
+
+export function getServerList(req: express.Request, res: express.Response) {
+    return simrailClient.get("servers-open", BASE_SIMRAIL_API)?.then((e) => {
+        return res
+            .setHeader("Cache-control", 'public, max-age=3600')
+            .send((e.data as ApiResponse<Server>).data);
+    });
+};
+
+export function getStationsList(req: express.Request, res: express.Response, serverCode: string) {
+    return simrailClient.get("stations-open?serverCode=" + serverCode)?.then((e) => {
+        return res
+            .setHeader("Cache-control", 'public, max-age=3600')
+            .send((e.data as ApiResponse<Station>).data);
+    });
+}
+
+export function getTrainsList(req: express.Request, res: express.Response, serverCode: string) {
+    return simrailClient.get("trains-open?serverCode=" + serverCode)?.then((e) => {
+        return res
+            .setHeader("Cache-control", 'public, max-age=10, must-revalidate, stale-if-error=30')
+            .send((e.data as ApiResponse<Train>).data);
+    });
+}
