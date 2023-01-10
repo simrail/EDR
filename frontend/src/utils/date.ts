b935aa46921d379ef94a40f4c172d1b161abf8d0
Tallyrald <mpeter007@gmail.com>
2023-01-10 19:10:03 +0100

    Nicer way of handling time formatting (#8)

--- frontend/src/utils/date.ts
+++ frontend/src/utils/date.ts
@@ -1,5 +1,11 @@
 import utcToZonedTime from 'date-fns-tz/utcToZonedTime'
 
+const options: Intl.DateTimeFormatOptions = {
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit'
+};
+
 export const nowUTC = (targetTimezone: string = "Europe/Paris") => {
     const now = new Date();
     return utcToZonedTime(Date.UTC(now.getUTCFullYear(),now.getUTCMonth(), now.getUTCDate() ,
@@ -8,15 +14,8 @@ export const nowUTC = (targetTimezone: string = "Europe/Paris") => {
 
 // We don't care about the date, only time
 export const formatTime = (date: Date, language: string) => {
-    if (date == null) {
+    if (!(date instanceof Date)) {
 		return '-';
 	}
-
-	const options: Intl.DateTimeFormatOptions = {
-		hour: '2-digit',
-		minute: '2-digit',
-        second: '2-digit'
-	};
-
 	return date.toLocaleTimeString(language, options);
 }
