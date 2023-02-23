50e6d4516e6cea084aa3a8636fc66443a5a84ac7
Alex Olari <queadx@gmail.com>
2023-02-23 14:26:44 +0200

    Sort the timetable from current hour with one hour earlier to next day (#64)
    * Sort the timetable from current hour with a delay of one hour to next day
    
    * small lint fixes
    
    * clean the code
    
    * Improve UX for table

--- frontend/src/utils/date.ts
+++ frontend/src/utils/date.ts
@@ -1,21 +1,21 @@
 import utcToZonedTime from 'date-fns-tz/utcToZonedTime'
 import {addDays, addHours} from "date-fns";
 
-const options: Intl.DateTimeFormatOptions = {
+export const timeOptions: Intl.DateTimeFormatOptions = {
     hour: '2-digit',
     minute: '2-digit',
     second: '2-digit',
     hour12: false
 };
  // TODO: Take UTC offset from API now. And then shift the hours by the UTC offset value instead of taking TZ
-export const nowUTC = (serverTzOffset: number = 0) => {
+export const nowUTC = (serverTzOffset: number = 0, addDelay?: number) => {
     const now = new Date();
     return addHours(utcToZonedTime(Date.UTC(now.getUTCFullYear(),now.getUTCMonth(), now.getUTCDate() ,
-        now.getUTCHours(), now.getUTCMinutes(), now.getUTCSeconds(), now.getUTCMilliseconds()), "Europe/London"), serverTzOffset)
+        addDelay ? now.getUTCHours() - addDelay : now.getUTCHours(), now.getUTCMinutes(), now.getUTCSeconds(), now.getUTCMilliseconds()), "Europe/London"), serverTzOffset)
 }
 
 // We don't care about the date, only time
-export const formatTime = (date: Date, language: string) => {
+export const formatTime = (date: Date, language: string, options?: Intl.DateTimeFormatOptions) => {
     if (!(date instanceof Date)) {
 		return '-';
 	}
