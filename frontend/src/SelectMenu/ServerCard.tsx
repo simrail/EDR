cfc056b4d4af6b4058088e2814af47debae70ef4
quead <queadx@gmail.com>
2023-02-23 17:39:45 +0200

    Update ui/ux for server list

--- frontend/src/SelectMenu/ServerCard.tsx
+++ frontend/src/SelectMenu/ServerCard.tsx
@@ -3,20 +3,31 @@ import {Card} from "flowbite-react/lib/esm/components/Card";
 import {countries} from "country-flag-icons";
 import {countriesFlags} from "../config";
 import {console_log} from "../utils/Logger";
-import {useNavigate} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 
 export const ServerCard: React.FC<any> = ({server}) => {
     const navigate = useNavigate();
     const serverCountryCode = server.ServerCode.slice(0, 2);
     console_log("c ", countries);
     const flag = countriesFlags[serverCountryCode.toUpperCase()];
+    const serverCardPath = server.ServerCode ? `/${server.ServerCode}` : '';
 
     console_log("falg", flag);
     return (
-        <Card className="m-4 cursor-pointer w-[150px] text-center" onClick={() => {
-            navigate("/" + server.ServerCode);
-        }} imgSrc={flag ? "data:image/svg+xml;base64, " + btoa(flag) : undefined}>
-            {server.ServerName}
-        </Card>
+        <>
+            <li className="w-[200px]">
+                <Link to={serverCardPath} className="flex items-center w-full h-full py-4">
+                    <div className="relative pr-2 pl-4">
+                        <span className={`absolute block w-2 h-2 left-0 rounded-full top-1/2 -translate-y-1/2 ${server.IsActive ? 'bg-green-400' : 'bg-red-600'}`}></span>
+                        <span className="child:w-4 child:h-auto" dangerouslySetInnerHTML={{ __html: flag.toString() }} />
+                    </div>
+                    <div className="min-w-0">
+                        <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
+                            {server.ServerName}
+                        </p>
+                    </div>
+                </Link>
+            </li>
+        </>
     )
 }
--- frontend/src/SelectMenu/ServerSelect.tsx
+++ frontend/src/SelectMenu/ServerSelect.tsx
@@ -25,14 +25,67 @@ export const ServerSelect: React.FC<Props> = ({isWebpSupported}) => {
         return language.includes(s.ServerCode.slice(0, 2).toUpperCase()) ? -1 : 0
     });
     console_log("servers: ", servers);
+    console.log("servers: ", orderedServers);
 
     return <SelectMenuLayout title={t("SELECTMENU_server_selection")} isWebpSupported={isWebpSupported}>
         {
             !orderedServers
                 ? <Spinner/>
-                : orderedServers.map((s) => {
-                    return <ServerCard key={s.ServerCode} server={s} size="xl"/>
-                })
+                : (
+                    <>
+                        <ul className="bg-white px-4 max-w-md divide-y divide-gray-200 rounded-lg shadow-md dark:divide-gray-700 m-4 dark:bg-gray-800">
+                            {orderedServers.filter(item => /^en/i.test(item.ServerCode)).map((s) => {
+                                return <ServerCard key={s.ServerCode} server={s} size="xl"/>
+                            })}
+                        </ul>
+                        <ul className="bg-white px-4 max-w-md divide-y divide-gray-200 rounded-lg shadow-md dark:divide-gray-700 m-4 dark:bg-gray-800">
+                            {orderedServers.filter(item => /^cn/i.test(item.ServerCode)).map((s) => {
+                                return <ServerCard key={s.ServerCode} server={s} size="xl"/>
+                            })}
+                        </ul>
+                        <ul className="bg-white px-4 max-w-md divide-y divide-gray-200 rounded-lg shadow-md dark:divide-gray-700 m-4 dark:bg-gray-800">
+                            {orderedServers.filter(item => /^cz/i.test(item.ServerCode)).map((s) => {
+                                return <ServerCard key={s.ServerCode} server={s} size="xl"/>
+                            })}
+                        </ul>
+                        <ul className="bg-white px-4 max-w-md divide-y divide-gray-200 rounded-lg shadow-md dark:divide-gray-700 m-4 dark:bg-gray-800">
+                            {orderedServers.filter(item => /^de/i.test(item.ServerCode)).map((s) => {
+                                return <ServerCard key={s.ServerCode} server={s} size="xl"/>
+                            })}
+                        </ul>
+                        <ul className="bg-white px-4 max-w-md divide-y divide-gray-200 rounded-lg shadow-md dark:divide-gray-700 m-4 dark:bg-gray-800">
+                            {orderedServers.filter(item => /^es/i.test(item.ServerCode)).map((s) => {
+                                return <ServerCard key={s.ServerCode} server={s} size="xl"/>
+                            })}
+                        </ul>
+                        <ul className="bg-white px-4 max-w-md divide-y divide-gray-200 rounded-lg shadow-md dark:divide-gray-700 m-4 dark:bg-gray-800">
+                            {orderedServers.filter(item => /^fr/i.test(item.ServerCode)).map((s) => {
+                                return <ServerCard key={s.ServerCode} server={s} size="xl"/>
+                            })}
+                        </ul>
+                        <ul className="bg-white px-4 max-w-md divide-y divide-gray-200 rounded-lg shadow-md dark:divide-gray-700 m-4 dark:bg-gray-800">
+                            {orderedServers.filter(item => /^hu/i.test(item.ServerCode)).map((s) => {
+                                return <ServerCard key={s.ServerCode} server={s} size="xl"/>
+                            })}
+                        </ul>
+                        <ul className="bg-white px-4 max-w-md divide-y divide-gray-200 rounded-lg shadow-md dark:divide-gray-700 m-4 dark:bg-gray-800">
+                            {orderedServers.filter(item => /^it/i.test(item.ServerCode)).map((s) => {
+                                return <ServerCard key={s.ServerCode} server={s} size="xl"/>
+                            })}
+                        </ul>
+                        <ul className="bg-white px-4 max-w-md divide-y divide-gray-200 rounded-lg shadow-md dark:divide-gray-700 m-4 dark:bg-gray-800">
+                            {orderedServers.filter(item => /^p/i.test(item.ServerCode)).map((s) => {
+                                return <ServerCard key={s.ServerCode} server={s} size="xl"/>
+                            })}
+                        </ul>
+                        <ul className="bg-white px-4 max-w-md divide-y divide-gray-200 rounded-lg shadow-md dark:divide-gray-700 m-4 dark:bg-gray-800">
+                            {orderedServers.filter(item => /^ua/i.test(item.ServerCode)).map((s) => {
+                                return <ServerCard key={s.ServerCode} server={s} size="xl"/>
+                            })}
+                        </ul>
+
+                    </>
+                )
         }
     </SelectMenuLayout>
 };
