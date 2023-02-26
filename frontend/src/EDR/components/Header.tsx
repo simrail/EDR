9c8cac04e2eb8619d0f470f29bbf45861b9920a2
quead <queadx@gmail.com>
2023-02-26 16:10:07 +0200

    add support for dark mode on sirius

--- frontend/src/EDR/components/Header.tsx
+++ frontend/src/EDR/components/Header.tsx
@@ -76,8 +76,8 @@ export const Header: React.FC<Props> = ({
                 <div className="flex flex-col">
                     <span>{postCfg.srId}</span>
                     <Link to={`/${serverCode}`} className="underline flex">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mr-1">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75" />
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mr-2">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 12h-15m0 0l6.75 6.75M4.5 12l6.75-6.75" />
                         </svg>
                         {!streamMode ? t('EDR_UI_back') : ''}
                     </Link>
--- frontend/src/Sirius/Header.tsx
+++ frontend/src/Sirius/Header.tsx
@@ -1,4 +1,7 @@
+import { DarkThemeToggle } from "flowbite-react";
 import React from "react";
+import { Link } from "react-router-dom";
+import { countriesFlags } from "../config";
 
 type Props = {
     trainNumber: string;
@@ -7,8 +10,24 @@ type Props = {
 }
 export const SiriusHeader: React.FC<Props> = ({trainNumber, trainDetails, serverCode}) => {
     return (
-        <div className="sticky z-20 t-0 shadow-md w-full h-[30px] flex items-center bg-white dark:bg-slate-800">
-            <div>{serverCode} - {trainNumber} - {trainDetails.Vehicles[0]}</div>
+        <div className="sticky z-20 px-2 t-0 shadow-md w-full h-[30px] flex columns-3 items-center bg-white dark:bg-slate-800">
+            <div className="w-full flex">
+                <Link to={`/${serverCode}/trains`} className="flex underline hover:no-underline">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mr-2">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 12h-15m0 0l6.75 6.75M4.5 12l6.75-6.75" />
+                    </svg>
+                    Back to Trains
+                </Link>
+            </div>
+            <div className="w-full flex justify-center items-center">
+                <span className="ml-2 mr-1 child:w-6 child:h-auto shadow-md" dangerouslySetInnerHTML={{ __html: countriesFlags[serverCode.slice(0, 2).toUpperCase()].toString() }} />
+                    <span className="font-bold mr-2">{serverCode.toUpperCase()}
+                </span>
+                - {trainNumber} - {trainDetails.Vehicles[0]}
+            </div>
+            <div className="w-full flex justify-end">
+                <DarkThemeToggle className="!p-0" />
+            </div>
         </div>
     )
 }
