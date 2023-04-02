4b03515eca21a0134d438e826344150e1b00542a
Peter Mohacsi <tally.github@gmail.com>
2023-04-02 10:44:50 +0200

    Add driver view back link to translations

--- frontend/src/Sirius/Header.tsx
+++ frontend/src/Sirius/Header.tsx
@@ -21,14 +21,14 @@ export const SiriusHeader: React.FC<Props> = ({trainNumber, trainDetails, server
     const {t} = useTranslation();
     return (
         <div className="sticky z-20 px-2 t-0 shadow-md w-full h-[40px] flex columns-3 items-center bg-white dark:bg-slate-800 overflow-x-auto justify-between">
-            <div className="flex max-h-[28px] w-max-content">
+            <div className="flex grow max-h-[28px] w-max-content">
                 <Link to={`/${serverCode}/trains`} className="flex underline hover:no-underline">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mr-2">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 12h-15m0 0l6.75 6.75M4.5 12l6.75-6.75" />
                     </svg>
-                    Back to Trains
+                    {t("DRIVER_DETAILS_back_link")}
                 </Link>
-                <div className="w-full justify-start items-center hidden md:flex">
+                <div className="w-content items-center hidden md:flex">
                     <span className="ml-2 mr-1 child:w-6 child:h-auto shadow-md" dangerouslySetInnerHTML={{ __html: countriesFlags[serverCode.slice(0, 2).toUpperCase()].toString() }} />
                         <span className="font-bold mr-2">{serverCode.toUpperCase()}
                     </span>
