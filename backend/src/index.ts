1ed37aefd121fadb98045c055d2de1a5bee93838
Peter Mohacsi <tally.github@gmail.com>
2023-05-21 14:56:43 +0200

    Fix version number

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -134,6 +134,6 @@ app
     .get("/steam/:steamId", getPlayer);
 app.listen(process.env.LISTEN_PORT);
 
-console.log("🚆 Simrail Community EDR backend v2.2");
+console.log("🚆 Simrail Community EDR backend v2.3");
 console.log("💻 https://github.com/simrail/EDR");
 console.log("🐛 https://github.com/simrail/EDR/issues")
